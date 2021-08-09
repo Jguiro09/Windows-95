b656abcbcc4f11b8af87dfa2e216fe980b99781d
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-08-09 11:21:33 -0500

    added README

--- controllers/api/sent-routes.js
+++ controllers/api/sent-routes.js
@@ -4,47 +4,43 @@ const { Sent } = require('../../models');
 // The `/api/sent` endpoint
 
 router.get('/', async(req, res) => {
-  try {
-    const allSent = await Sent.findAll();
-    res.status(200).json(allSent);
-  } 
-  catch (err) {
-    res.status(500).json(err);
-  }
+    try {
+        const allSent = await Sent.findAll();
+        res.status(200).json(allSent);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.get('/:id', async (req, res) => {
-  try {
-    const findSent = await Sent.findByPk(req.params.id);
-    res.status(200).json(findSent);
-  } 
-  catch (err) {
-    res.status(500).json(err);
-  }
+router.get('/:id', async(req, res) => {
+    try {
+        const findSent = await Sent.findByPk(req.params.id);
+        res.status(200).json(findSent);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.post('/', async (req, res) => {
-  try {
-    const newSent = await Sent.create(req.body);
-    res.status(200).json(newSent);
-  }
-  catch (err) {
-    res.status(400).json(err);
-  }
+router.post('/', async(req, res) => {
+    try {
+        const newSent = await Sent.create(req.body);
+        res.status(200).json(newSent);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 router.put('/:id', (req, res) => {
-  try {
-    const updateSent = Sent.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.status(200).json(updateSent);
-  } 
-  catch (err) {
-    res.status(500).json(err);
-  }
+    try {
+        const updateSent = Sent.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+        res.status(200).json(updateSent);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
